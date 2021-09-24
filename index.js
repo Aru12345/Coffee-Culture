a4019eac959ec013bcf03b3e37a05399b2f729b7
Arundhati <arundhatiraicar12@gmail.com>
2021-09-25 01:29:09 +0530

    final

--- index.js
+++ index.js
@@ -1,58 +1,28 @@
 
-const BASE_URL="http://localhost:3000/coffee"
-const container=document.getElementById("coffee-list")
+const BASE_URL="http://localhost:3000/coffee";
+const container=document.getElementById("coffee-list");
 const searchBar=document.getElementById("searchBar");
-const recipeSubmit=document.getElementById("recipeSubmit")
+const recipeSubmit=document.getElementById("recipeSubmit");
 const addForm=document.getElementById("addForm");
-
-//console.log(searchBar)
-
-addForm.addEventListener("submit",(event)=>{
-  event.preventDefault();
-  const [title,image,type,description,ingredients,recipe,likes]=event.target
-  fetch(" http://localhost:3000/coffee", {
-        method: "POST", 
-        headers: {
-            "Content-Type": "application/json"
-        }, 
-        body: JSON.stringify({
-            title:title.value,
-            image:image.value,
-            type:type.value,
-            ingredients:ingredients.value,
-            recipe:recipe.value,
-            description:description.value,
-            likes:likes.value
-        })
-    })
-    .then(response => response.json())
-    .then(response => console.log(response))
-    addForm.reset()
-})
-addForm.addEventListener("submit",()=>{
-  alert("Your recipe has been submited")
-})
-
-function search_coffee() {
-  let input = document.getElementById('searchBar').value
-  input=input.toLowerCase();
-  //let x = document.getElementsByClassName(`coffee-card`);
-    debugger;
-  for (i = 0; i < copy.length; i++) { 
-      if (!copy[i].innerHTML.toLowerCase().includes(input)) {
-          copy[i].style.display="none";
-      }
-      else {
-          copy[i].style.display="list-item";                 
-      }
-  }
-}
-searchBar.addEventListener('keyup',search_coffee)
-
-
-//console.log(container)
-//json-server --watch db.json
+const forward=document.getElementById("forward");
+const back =document.getElementById("#back");
 let pageNumber =1
+//json-server --watch db.json
+addForm.addEventListener('submit',(e)=>{
+  e.preventDefault()
+  window.scrollTo(0, 0);
+})
+
+searchBar.addEventListener("keyup",search_coffee)
+
+function getCoffee(){
+  fetch(`http://localhost:3000/coffee?_limit=15&_page=${pageNumber}`)
+  .then(resp=>resp.json())
+  .then(data=>{
+      data.forEach(renderCoffee)
+      })
+  }
+
 function renderCoffee(coffee){
     const div=document.createElement("div")
     div.id="coffeeCard-${coffee.id}"
@@ -95,6 +65,47 @@ function renderCoffee(coffee){
     container.appendChild(div)
 }
 
+function createCoffee(event) {
+  event.preventDefault();
+  const title = document.querySelector("#title").value;
+  const image = event.target.querySelector("#image").value;
+  const description=document.querySelector("#description").value;
+  const type=document.querySelector("#type").value;
+  const ingredients=document.querySelector('#ingredients').value
+  const recipe=document.querySelector("#recipe").value
+
+  const coffee= {
+    title:title,
+    image:image,
+    description:description,
+    type:type,
+    ingredients:ingredients,
+    recipe:recipe,
+    likes: 0,
+  };
+
+  const configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(coffee),
+  };
+
+  fetch(BASE_URL, configObj)
+    .then(function (resp) {
+      return resp.json();
+    })
+    .then(function (coffee) {
+      renderCoffee(coffee);
+    });
+  addForm.reset();
+}
+addForm.addEventListener("submit",()=>{
+  alert("Your recipe is published.") 
+})
+
+
 
 
 function increaseLikes(coffee, likesNum) {
@@ -109,15 +120,6 @@ function increaseLikes(coffee, likesNum) {
     body: JSON.stringify({ likes: coffee.likes }), 
   });
   
-}
-
-function getCoffee(){
-fetch(`http://localhost:3000/coffee?_limit=3&_page=${pageNumber}`)
-.then(resp=>resp.json())
-.then(data=>{
-    data.forEach(renderCoffee)
-   
-})
 }
 
 document.addEventListener("click",(event)=>{
@@ -135,33 +137,42 @@ const handleFrontClick = (event) => {
 
   pageNumber += 1
   const coffeeList = document.querySelector('#coffee-list')
-  debugger;
   coffeeList.innerHTML = ""
-  
+ 
   getCoffee()
+  window.scrollTo(0, 0);
 
 }
 const handleBackClick=(event)=>{
-  
+
   pageNumber-=1
   const coffeeList=document.querySelector("#coffee-list")
   coffeeList.innerHTML=""
-  
+  window.scrollTo(0, 0);
   getCoffee()
 }
 
-
-let copy
+function search_coffee() {
+  let input = document.getElementById('searchBar').value
+  input=input.toLowerCase();
+  let x = document.getElementsByClassName('coffee-card');
+     
+  for (i = 0; i < x.length; i++) { 
+      if (!x[i].innerHTML.toLowerCase().includes(input)) {
+          x[i].style.display="none";
+      }
+      else {
+          x[i].style.display="list-item";                 
+      }
+  }
+}
 document.addEventListener("DOMContentLoaded",(event)=>{
+
+   getCoffee()
+  addForm.addEventListener("submit", createCoffee);
   
-  fetch('http://localhost:3000/coffee')
-  .then(resp=>resp.json())
-  .then(data=>{
-    copy=data
-    
-  })
-
-
-  getCoffee()
-
 })
+
+
+
+
